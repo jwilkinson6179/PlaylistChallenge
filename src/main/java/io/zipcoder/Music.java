6ac8558572ba99535a61640a24f06d33ab4a8b55
James Wilkinson <JamesW6179@gmail.com>
2020-02-15 02:11:49 -0500

    Modulo needs fixing for backwards clicking

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,49 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        Integer forwards = forwardClicks(startIndex, selection);
+        Integer backwards = backwardsClicks(startIndex, selection);
+
+        if(forwards <= backwards) {
+            return forwards;
+        }
+        else
+        {
+            return backwards;
+        }
+    }
+
+    public Integer forwardClicks(Integer startIndex, String selection)
+    {
+        for(int clicks = 0; clicks < playList.length; clicks++)
+        {
+            int currentTrack = (startIndex + clicks) % playList.length;
+            if(playList[currentTrack].equals(selection))
+            {
+                return clicks;
+            }
+        }
+        return playList.length;
+    }
+
+    public Integer backwardsClicks(Integer startIndex, String selection)
+    {
+        int clicks = 0;
+        for(int i = playList.length - 1; i >= 0; i++)
+        {
+            int currentTrack = Math.abs((startIndex - clicks) % playList.length);
+            if(playList[currentTrack].equals(selection))
+            {
+                return clicks;
+            }
+            clicks++;
+        }
+        return playList.length;
     }
 }
+
+
+// Two helper functions
+// Iterate forward, looking for matching String value.  If Starting position + buttonClicks >= songlist length, search
+// wraps to 0 via (start + buttonClicks) % (playlist.length)
+// Iterate backwards similarly, except if start - buttonClicks < 0, (start - buttonClicks) % playlist.length
--- src/test/java/io/zipcoder/MusicTest.java
+++ src/test/java/io/zipcoder/MusicTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 public class MusicTest {
 
     @Test
-    public void selectionTest1(){
+    public void selectionTest1() {
 
-        String[] playlist = {"wheniseeyouagain","borntorun","nothingelsematters","cecelia"};
+        String[] playlist = {"wheniseeyouagain", "borntorun", "nothingelsematters", "cecelia"};
         Music music = new Music(playlist);
         Integer startingIndex = 1;
         String selection = "cecelia";
@@ -18,14 +18,92 @@ public class MusicTest {
     }
 
     @Test
-    public void selectionTest2(){
+    public void selectionTest2() {
 
-        String[] playlist = {"dancinginthedark","rio","liveoak","liveoak"};
+        String[] playlist = {"dancinginthedark", "rio", "liveoak", "liveoak"}; // len = 4, start = 1,
         Music music = new Music(playlist);
         Integer startingIndex = 0;
         String selection = "liveoak";
         Integer expected = 1;
         Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void forwardClicks() {
+        String[] playlist = {"norman $%!* rockwell", "mariners apartment complex", "venice", "doing time", "cinnamon girl", "how to disappear", "bartender"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 0;
+        String selection = "norman $%!* rockwell";
+        Integer expected = 0;
+        Integer actual = music.forwardClicks(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void forwardClicksThree() {
+        String[] playlist = {"norman $%!* rockwell", "mariners apartment complex", "venice", "doing time", "cinnamon girl", "how to disappear", "bartender"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 2;
+        String selection = "how to disappear";
+        Integer expected = 3;
+        Integer actual = music.forwardClicks(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void forwardClicksWraps() {
+        String[] playlist = {"norman $%!* rockwell", "mariners apartment complex", "venice", "doing time", "cinnamon girl", "how to disappear", "bartender"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 5;
+        String selection = "venice";
+        Integer expected = 4;
+        Integer actual = music.forwardClicks(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void backwardsClicks() {
+        String[] playlist = {"jingle bells", "frosty", "rudolph", "merry christmas"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 1;
+        String selection = "frosty";
+        Integer expected = 0;
+        Integer actual = music.forwardClicks(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void backwardsClicksTwo() {
+        String[] playlist = {"north", "east", "west", "south", "left", "right", "up", "down"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 5;
+        String selection = "west";
+        Integer expected = 3;
+        Integer actual = music.backwardsClicks(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void backwardsClicksWrap() {
+        String[] playlist = {"mgs", "zelda", "mario", "corax", "crash", "metroid"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 1;
+        String selection = "crash";
+        Integer expected = 3;
+        Integer actual = music.forwardClicks(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void backwardsWrapTwo() {
+
+        String[] playlist = {"pine", "elm", "oak", "oak"}; // len = 4, start = 1,
+        Music music = new Music(playlist);
+        Integer startingIndex = 0;
+        String selection = "oak";
+        Integer expected = 1;
+        Integer actual = music.backwardsClicks(startingIndex, selection);
         Assert.assertEquals(expected, actual);
     }
 }
