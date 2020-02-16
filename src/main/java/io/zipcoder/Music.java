d36f3234249ea9cf79e19251bc96787797588494
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-16 17:05:51 -0500

    One loop

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -8,67 +8,15 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        Integer forwards = forwardClicks(startIndex, selection);
-        Integer backwards = backwardClicks(startIndex, selection);
+    public Integer selection(Integer startIndex, String selection) {
+        for(int clicks = 0; clicks < playList.length; clicks++) {
 
-        if(forwards <= backwards) {
-            return forwards;
-        }
-        else
-        {
-            return backwards;
-        }
-    }
-
-    public Integer forwardClicks(Integer startIndex, String selection) {
-        for (int clicks = 0; clicks < playList.length; clicks++) {
-            int currentTrack = (startIndex + clicks) % playList.length;
-            if (playList[currentTrack].equals(selection)) {
+            if (playList[(startIndex + clicks + playList.length) % playList.length].equals(selection) ||
+                    playList[(startIndex - clicks + playList.length) % playList.length].equals(selection)) {
                 return clicks;
             }
         }
-        return playList.length + 1;
-    }
 
-    public Integer backwardClicks(Integer startIndex, String selection) {
-        int clickCount = 0;
-
-        for(int i = playList.length - 1; i >= 0; i--)
-        {
-            int currentTrack = (startIndex - clickCount);
-            if(currentTrack < 0)
-            {
-                currentTrack = playList.length - 1 + currentTrack;
-            }
-
-            if(playList[currentTrack].equals(selection))
-            {
-                return clickCount;
-            }
-
-            clickCount++;
-        }
-
-        return playList.length + 1;
-    }
-
-
-    public String[] reversePlaylist()
-    {
-        String[] reversedPlaylist = new String[playList.length];
-
-        for(Integer i = playList.length - 1; i >= 0; i--)
-        {
-            reversedPlaylist[(playList.length - 1) - i] = playList[i];
-        }
-
-        return reversedPlaylist;
+        return -1;
     }
 }
-
-
-// Two helper functions
-// Iterate forward, looking for matching String value.  If Starting position + buttonClicks >= songlist length, search
-// wraps to 0 via (start + buttonClicks) % (playlist.length)
-// Iterate backwards similarly, except if start - buttonClicks < 0, (start - buttonClicks) % playlist.length
--- src/test/java/io/zipcoder/MusicTest.java
+++ src/test/java/io/zipcoder/MusicTest.java
@@ -30,85 +30,14 @@ public class MusicTest {
     }
 
     @Test
-    public void forwardClicks() {
-        String[] playlist = {"norman $%!* rockwell", "mariners apartment complex", "venice", "doing time", "cinnamon girl", "how to disappear", "bartender"};
+    public void selectionTest3() {
+
+        String[] playlist = {"dancinginthedark", "rio", "liveoak", "liveoak", "test", "liveoak"}; // len = 4, start = 1,
         Music music = new Music(playlist);
         Integer startingIndex = 0;
-        String selection = "norman $%!* rockwell";
-        Integer expected = 0;
-        Integer actual = music.forwardClicks(startingIndex, selection);
+        String selection = "liveoak";
+        Integer expected = 1;
+        Integer actual = music.selection(startingIndex, selection);
         Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void forwardClicksThree() {
-        String[] playlist = {"norman $%!* rockwell", "mariners apartment complex", "venice", "doing time", "cinnamon girl", "how to disappear", "bartender"};
-        Music music = new Music(playlist);
-        Integer startingIndex = 2;
-        String selection = "how to disappear";
-        Integer expected = 3;
-        Integer actual = music.forwardClicks(startingIndex, selection);
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void forwardClicksWraps() {
-        String[] playlist = {"norman $%!* rockwell", "mariners apartment complex", "venice", "doing time", "cinnamon girl", "how to disappear", "bartender"};
-        Music music = new Music(playlist);
-        Integer startingIndex = 5;
-        String selection = "venice";
-        Integer expected = 4;
-        Integer actual = music.forwardClicks(startingIndex, selection);
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void backwardsClicks() {
-        String[] playlist = {"jingle bells", "frosty", "rudolph", "merry christmas"};
-        Music music = new Music(playlist);
-        Integer startingIndex = 1;
-        String selection = "frosty";
-        Integer expected = 0;
-        Integer actual = music.forwardClicks(startingIndex, selection);
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void backwardsClicksWrap() {
-        String[] playlist = {"mgs", "zelda", "mario", "corax", "crash", "metroid"};
-        Music music = new Music(playlist);
-        Integer startingIndex = 1;
-        String selection = "crash";
-        Integer expected = 3;
-        Integer actual = music.forwardClicks(startingIndex, selection);
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void reverseListTest() {
-        // GIVEN
-        String[] playlist = { "aa", "bb", "cc", "dd", "ee" };
-        String[] expected = { "ee", "dd", "cc", "bb", "aa" };
-        Music music = new Music(playlist);
-
-        // WHEN
-        String[] actual = music.reversePlaylist();
-
-        // THEN
-        Assert.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void reverseListTestRepeatEntries() {
-
-        String[] playlist = {"aaa", "bbb", "ccc", "ccc"};
-        String[] expected = { "ccc", "ccc", "bbb", "aaa" };
-        Music music = new Music(playlist);
-
-        // WHEN
-        String[] actual = music.reversePlaylist();
-
-        // THEN
-        Assert.assertArrayEquals(expected, actual);
     }
 }
